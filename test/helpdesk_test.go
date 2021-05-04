cafe8992f40ae035d55278e7551e8e05e7bb151c
chyroc <chyroc@qq.com>
2021-05-04 12:36:06 +0800

    test: more test

---
+++ test/helpdesk_test.go
@@ -0,0 +1,43 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/chyroc/lark"
+)
+
+func Test_HelpDesk(t *testing.T) {
+	as := assert.New(t)
+
+	t.Run("request failed", func(t *testing.T) {
+		cli := AppALLPermission.Ins()
+		cli.Mock().MockGetTenantAccessToken(mockGetTenantAccessTokenFailed)
+		helpdeskCli := cli.HelpDesk()
+
+		t.Run("", func(t *testing.T) {
+			_, _, err := helpdeskCli.StartService(ctx, &lark.StartServiceReq{})
+			as.NotNil(err)
+			as.Equal(err.Error(), "failed")
+		})
+
+		t.Run("", func(t *testing.T) {
+			_, _, err := helpdeskCli.GetTicket(ctx, &lark.GetTicketReq{})
+			as.NotNil(err)
+			as.Equal(err.Error(), "failed")
+		})
+
+		t.Run("", func(t *testing.T) {
+			_, _, err := helpdeskCli.GetTicketList(ctx, &lark.GetTicketListReq{})
+			as.NotNil(err)
+			as.Equal(err.Error(), "failed")
+		})
+
+		t.Run("", func(t *testing.T) {
+			_, _, err := helpdeskCli.UpdateTicket(ctx, &lark.UpdateTicketReq{})
+			as.NotNil(err)
+			as.Equal(err.Error(), "failed")
+		})
+	})
+}
