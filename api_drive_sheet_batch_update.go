336818fdbe08f08ab89ca6afef625188ac551fd2
chyroc <chyroc@qq.com>
2021-05-25 19:30:30 +0800

    codegen: fix update-sheet api

--- api_drive_sheet_batch_update.go
+++ api_drive_sheet_batch_update.go
@@ -40,39 +40,39 @@ func (r *Mock) UnMockDriveBatchUpdateSheet() {
 }
 
 type BatchUpdateSheetReq struct {
-	SpreadSheetToken string                       `path:"spreadsheetToken" json:"-"` // spreadsheet 的 token，获取方式见[在线表格开发指南](/ssl:ttdoc/ukTMukTMukTM/uATMzUjLwEzM14CMxMTN/overview)
-	Requests         *BatchUpdateSheetReqRequests `json:"requests,omitempty"`        // 请求操作，支持增、删、复制工作表 ，三个操作选一个
+	SpreadSheetToken string                        `path:"spreadsheetToken" json:"-"` // spreadsheet 的 token，获取方式见[在线表格开发指南](/ssl:ttdoc/ukTMukTMukTM/uATMzUjLwEzM14CMxMTN/overview)
+	Requests         []*BatchUpdateSheetReqRequest `json:"requests,omitempty"`        // 请求操作，支持增、删、复制工作表 ，三个操作选一个
 }
 
-type BatchUpdateSheetReqRequests struct {
-	AddSheet    *BatchUpdateSheetReqRequestsAddSheet    `json:"addSheet,omitempty"`    // 增加工作表
-	CopySheet   *BatchUpdateSheetReqRequestsCopySheet   `json:"copySheet,omitempty"`   // 复制工作表
-	DeleteSheet *BatchUpdateSheetReqRequestsDeleteSheet `json:"deleteSheet,omitempty"` // 删除 sheet
+type BatchUpdateSheetReqRequest struct {
+	AddSheet    *BatchUpdateSheetReqRequestAddSheet    `json:"addSheet,omitempty"`    // 增加工作表
+	CopySheet   *BatchUpdateSheetReqRequestCopySheet   `json:"copySheet,omitempty"`   // 复制工作表
+	DeleteSheet *BatchUpdateSheetReqRequestDeleteSheet `json:"deleteSheet,omitempty"` // 删除 sheet
 }
 
-type BatchUpdateSheetReqRequestsAddSheet struct {
-	Properties *BatchUpdateSheetReqRequestsAddSheetProperties `json:"properties,omitempty"` // 工作表属性
+type BatchUpdateSheetReqRequestAddSheet struct {
+	Properties *BatchUpdateSheetReqRequestAddSheetProperties `json:"properties,omitempty"` // 工作表属性
 }
 
-type BatchUpdateSheetReqRequestsAddSheetProperties struct {
+type BatchUpdateSheetReqRequestAddSheetProperties struct {
 	Title string `json:"title,omitempty"` // 工作表标题
 	Index *int64 `json:"index,omitempty"` // 新增工作表的位置，不填默认往前增加工作表
 }
 
-type BatchUpdateSheetReqRequestsCopySheet struct {
-	Source      *BatchUpdateSheetReqRequestsCopySheetSource      `json:"source,omitempty"`      // 需要复制的工作表资源
-	Destination *BatchUpdateSheetReqRequestsCopySheetDestination `json:"destination,omitempty"` // 工作表 的属性
+type BatchUpdateSheetReqRequestCopySheet struct {
+	Source      *BatchUpdateSheetReqRequestCopySheetSource      `json:"source,omitempty"`      // 需要复制的工作表资源
+	Destination *BatchUpdateSheetReqRequestCopySheetDestination `json:"destination,omitempty"` // 工作表 的属性
 }
 
-type BatchUpdateSheetReqRequestsCopySheetSource struct {
+type BatchUpdateSheetReqRequestCopySheetSource struct {
 	SheetID string `json:"sheetId,omitempty"` // 源 sheetId
 }
 
-type BatchUpdateSheetReqRequestsCopySheetDestination struct {
+type BatchUpdateSheetReqRequestCopySheetDestination struct {
 	Title *string `json:"title,omitempty"` // 目标工作表名称。不填为 old_title(副本_0)
 }
 
-type BatchUpdateSheetReqRequestsDeleteSheet struct {
+type BatchUpdateSheetReqRequestDeleteSheet struct {
 	SheetID string `json:"sheetId,omitempty"` // sheetId
 }
 
