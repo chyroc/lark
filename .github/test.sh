6c7a19d396474634aaf062e7044833be47bdb707
chyroc <chyroc@qq.com>
2021-10-26 17:17:48 +0800

    :white_check_mark: test: fix test: rerun test

--- .github/test.sh
+++ .github/test.sh
@@ -2,12 +2,22 @@
 
 set -e
 
+# setup env
+export LOCAL_TEST=true
+export GO111MODULE=on
+
 module_name=$(cat go.mod | grep module | cut -d ' ' -f 2-2)
+module_list=(`go list ./...`)
 echo "module_name is $module_name"
 
-GO111MODULE=on echo 'mode: atomic' > coverage.txt && \
-  go list ./... | \
-    xargs -n1 -I{} sh -c 'LOCAL_TEST=true go test -covermode=atomic -coverprofile=coverage.tmp -coverpkg=./... -parallel 1 -p 1 -count=1 -gcflags=-l {} && tail -n +2 coverage.tmp >> coverage.txt' && \
-  rm coverage.tmp
+echo 'mode: atomic' > coverage.txt
+
+for ele in "${module_list[@]}";
+do
+  echo "start handle sub_module: $ele"
+  go test -covermode=atomic -coverprofile=coverage.tmp -coverpkg=./... -parallel 1 -p 1 -count=1 -gcflags=-l $ele | rerun-fail-test -retry-times=5 -- -covermode=atomic -coverprofile=coverage.tmp -coverpkg=./... -parallel 1 -p 1 -count=1 -gcflags=-l $ele
+  tail -n +2 coverage.tmp >> coverage.txt || echo ""
+  rm coverage.tmp || echo ""
+done
 
 # go tool cover -func=coverage.txt -o coverage.txt
