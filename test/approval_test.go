098165c62583340366e9058a3378f0b52186247a
MelindaY <yavejing@gmail.com>
2022-08-02 14:36:30 +0800

    fix: CreateApprovalInstance params dismatch (#60)
    Co-authored-by: zhaojingjing <zhaojingjing.1993@bytedance.com>

--- test/approval_test.go
+++ test/approval_test.go
@@ -125,6 +125,41 @@ func Test_UnmarshalGetApprovalInstance(t *testing.T) {
 	printData(res)
 }
 
+func Test_UnmarshalCreateApproval(t *testing.T) {
+	as := assert.New(t)
+
+	s := `{
+    "approval_code":"4202AD96-9EC1-4284-9C48-B923CDC4F30B",
+    "user_id":"59a92c4a",
+    "open_id":"ou_806a18fb5bdf525e38ba219733bdbd73",
+    "form":"[{\"id\":\"111\",\"type\":\"input\",\"value\":\"11111\"},{\"id\":\"222\",\"required\":true,\"type\":\"dateInterval\",\"value\":{\"start\":\"2019-10-01T08:12:01+08:00\",\"end\":\"2019-10-02T08:12:01+08:00\",\"interval\": 2.0}},{\"id\":\"333\",\"type\":\"radioV2\",\"value\":\"k2b8mkx0-h71x5gljn3i-1\"},{\"id\":\"444\",\"type\":\"number\", \"value\":\"4\"},{\"id\":\"555\",\"type\":\"textarea\",\"value\":\"fsafs\"}]",
+    "node_approver_user_id_list":[
+        {"key": "46e6d96cfa756980907209209ec03b64","value":["59a92c4a"]},
+        {"key": "manager_node_id","value":["59a92c4a"]}
+    ],
+    "node_approver_open_id_list":[
+        {"key": "46e6d96cfa756980907209209ec03b64","value":["ou_806a18fb5bdf525e38ba219733bdbd73","ou_806a18f"]},
+        {"key": "manager_node_id","value":["ou_806a18fb5bdf525e38ba219733bdbd73"]}
+    ],
+    "node_cc_user_id_list":[
+        {"key": "46e6d96cfa756980907209209ec03b64","value":["59a92c4a"]},
+        {"key": "manager_node_id","value":["59a92c4a"]}
+    ],
+    "node_cc_open_id_list":[
+        {"key": "46e6d96cfa756980907209209ec03b64","value":["ou_806a18fb5bdf525e38ba219733bdbd73"]},
+        {"key": "manager_node_id","value":["ou_806a18fb5bdf525e38ba219733bdbd73"]}
+    ]
+}
+`
+	res := new(lark.CreateApprovalInstanceReq)
+	err := json.Unmarshal([]byte(s), res)
+	as.Nil(err)
+	as.NotNil(res)
+	as.Equal("46e6d96cfa756980907209209ec03b64", res.NodeApproverOpenIDList[0].Key)
+	as.Equal("ou_806a18f", res.NodeApproverOpenIDList[0].Value[1])
+	printData(res)
+}
+
 func Test_Create_CancelApproval(t *testing.T) {
 	as := assert.New(t)
 
