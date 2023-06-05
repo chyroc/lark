cc28c35f59dfc018070ab63ffe250c5f2339a3ef
chyroc <chyroc@qq.com>
2023-06-05 12:54:50 +0800

    add: GetCoreHRSecurityGroupBP

--- api_corehr_security_group_list.go
+++ api_corehr_security_group_list.go
@@ -21,9 +21,9 @@ import (
 	"context"
 )
 
-// GetCoreHrSecurityGroupList 通过部门或工作地点, 查询对应的 HRBP/属地 BP。
+// GetCoreHrSecurityGroupList 批量查询「飞书人事」-「权限设置」-「角色设置」中的角色列表。
 //
-// doc: https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/corehr-v1/security_group/query
+// doc: https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/corehr-v1/security_group/list
 func (r *CoreHrService) GetCoreHrSecurityGroupList(ctx context.Context, request *GetCoreHrSecurityGroupListReq, options ...MethodOptionFunc) (*GetCoreHrSecurityGroupListResp, *Response, error) {
 	if r.cli.mock.mockCoreHrGetCoreHrSecurityGroupList != nil {
 		r.cli.log(ctx, LogLevelDebug, "[lark] CoreHr#GetCoreHrSecurityGroupList mock enable")
@@ -33,8 +33,8 @@ func (r *CoreHrService) GetCoreHrSecurityGroupList(ctx context.Context, request 
 	req := &RawRequestReq{
 		Scope:                 "CoreHr",
 		API:                   "GetCoreHrSecurityGroupList",
-		Method:                "POST",
-		URL:                   r.cli.openBaseURL + "/open-apis/corehr/v1/security_groups/query",
+		Method:                "GET",
+		URL:                   r.cli.openBaseURL + "/open-apis/corehr/v1/security_groups",
 		Body:                  request,
 		MethodOption:          newMethodOption(options),
 		NeedTenantAccessToken: true,
@@ -57,27 +57,37 @@ func (r *Mock) UnMockCoreHrGetCoreHrSecurityGroupList() {
 
 // GetCoreHrSecurityGroupListReq ...
 type GetCoreHrSecurityGroupListReq struct {
-	DepartmentIDType *DepartmentIDType                    `query:"department_id_type" json:"-"` // 此次调用中使用的部门 ID 类型, 示例值: people_corehr_department_id, 可选值有: open_department_id: 以 open_department_id 来标识部门, department_id: 以 department_id 来标识部门, people_corehr_department_id: 以 people_corehr_department_id 来标识部门, 默认值: `people_corehr_department_id`
-	ItemList         []*GetCoreHrSecurityGroupListReqItem `json:"item_list,omitempty"`          // 角色列表, 一次最多支持查询 50 个
-}
-
-// GetCoreHrSecurityGroupListReqItem ...
-type GetCoreHrSecurityGroupListReqItem struct {
-	RoleKey        string  `json:"role_key,omitempty"`         // 角色类型的唯一标识, HRBP: 与部门有关, role_key 固定为 「hrbp」, 属地 BP: 与部门、工作地点有关, role_key 固定为 「location_bp」, 示例值: "location_bp"
-	DepartmentID   string  `json:"department_id,omitempty"`    // 部门 ID, 查询 HRBP 需输入部门 ID, 示例值: "7063072995761456670"
-	WorkLocationID *string `json:"work_location_id,omitempty"` // 工作地点 ID, 查询属地 BP 需要输入部门 ID 与 工作地点 ID, 示例值: "6892687221355185677"
+	PageToken *string `query:"page_token" json:"-"` // 分页标记, 第一次请求不填, 表示从头开始遍历；分页查询结果还有更多项时会同时返回新的 page_token, 下次遍历可采用该 page_token 获取查询结果, 示例值: 10
+	PageSize  int64   `query:"page_size" json:"-"`  // 分页大小, 示例值: 100
 }
 
 // GetCoreHrSecurityGroupListResp ...
 type GetCoreHrSecurityGroupListResp struct {
-	HrbpList []*GetCoreHrSecurityGroupListRespHrbp `json:"hrbp_list,omitempty"` // HRBP/属地 BP 信息
+	Items     []*GetCoreHrSecurityGroupListRespItem `json:"items,omitempty"`      // 查询的用户角色信息
+	HasMore   bool                                  `json:"has_more,omitempty"`   // 是否还有更多项
+	PageToken string                                `json:"page_token,omitempty"` // 分页标记, 当 has_more 为 true 时, 会同时返回新的 page_token, 否则不返回 page_token
 }
 
-// GetCoreHrSecurityGroupListRespHrbp ...
-type GetCoreHrSecurityGroupListRespHrbp struct {
-	EmploymentIDList []string `json:"employment_id_list,omitempty"` // HRBP/属地 BP 的雇员ID : 对于 HRBP 而言, 若入参的部门没有找到对应的 HRBP, 将向上找寻, 即向其上级部门取对应的 HRBP, 且同一部门可能有多个 HRBP；, 对于 属地 BP 而言, 若入参的部门和地点没有找到对应的属地 BP, 将优先拿地点向上找寻, 即向其上级地点取对应的属地 BP
-	DepartmentID     string   `json:"department_id,omitempty"`      // 部门 ID
-	WorkLocationID   string   `json:"work_location_id,omitempty"`   // 工作地点 ID
+// GetCoreHrSecurityGroupListRespItem ...
+type GetCoreHrSecurityGroupListRespItem struct {
+	ID           string                                         `json:"id,omitempty"`            // 角色ID
+	Code         string                                         `json:"code,omitempty"`          // 角色code, 通常用于与其他系统进行交互
+	Name         *GetCoreHrSecurityGroupListRespItemName        `json:"name,omitempty"`          // 角色名称
+	ActiveStatus int64                                          `json:"active_status,omitempty"` // 状态, 1 = Inactive / 停用, 2 = Active / 启用, 3 = TobeActivated / 待启用
+	Description  *GetCoreHrSecurityGroupListRespItemDescription `json:"description,omitempty"`   // 角色描述
+	UpdateTime   string                                         `json:"update_time,omitempty"`   // 更新时间
+}
+
+// GetCoreHrSecurityGroupListRespItemDescription ...
+type GetCoreHrSecurityGroupListRespItemDescription struct {
+	ZhCn string `json:"zh_cn,omitempty"` // 中文
+	EnUs string `json:"en_us,omitempty"` // 英文
+}
+
+// GetCoreHrSecurityGroupListRespItemName ...
+type GetCoreHrSecurityGroupListRespItemName struct {
+	ZhCn string `json:"zh_cn,omitempty"` // 中文
+	EnUs string `json:"en_us,omitempty"` // 英文
 }
 
 // getCoreHrSecurityGroupListResp ...
